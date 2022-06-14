b9c925ef36f2603d9d40087e122d2b742b435766
Saddam Hossain <info.saddamxm@gmail.com>
2022-06-14 21:39:31 +0200

    CRUD operation implemented

--- server.py
+++ server.py
@@ -19,7 +19,39 @@ except:
     print("ERROR - Cannot connect to db")
 ########################################################
 
-@app.route("/users", methods=["GET"])
+# Data insert operation
+
+@app.route('/insert_users', methods=['POST'])
+def create_user():
+    try:
+        user = {
+            "firstName":request.form["firstName"],
+            "lastName": request.form["lastName"],
+            "email": request.form["email"],
+            "mobile": request.form["mobile"]
+        }
+        dbResponse = db.users.insert_one(user)
+        print(dbResponse.inserted_id)
+        # for attr in dir(dbResponse):
+        #     print(attr)
+        return Response(
+            response= json.dumps(
+                {"message":"user created",
+                 "id":f"{dbResponse.inserted_id}"
+                 }),
+            status=200,
+            mimetype="application/json"
+        )
+    except Exception as ex:
+
+        print(ex)
+        return Response(response= json.dumps({"message":"sorry! unable to insert the data"}), status=500, mimetype="application/json")
+
+
+
+# Retrive operation
+
+@app.route("/get_users", methods=["GET"])
 def get_some_users():
     try:
         data = list(db.users.find())
@@ -35,33 +67,53 @@ def get_some_users():
         return Response(response= json.dumps({"message":"cannot read users"}), status=500, mimetype="application/json")
 
 
-###########################################
 
-@app.route('/users', methods=['POST'])
-def create_user():
+# Update operation
+
+@app.route("/update_users/<id>", methods=["PATCH"])
+def update_users(id):
     try:
-        user = {
-            "name":request.form["name"],
-            "lastName": request.form["lastName"]
-        }
-        dbResponse = db.users.insert_one(user)
-        print(dbResponse.inserted_id)
-        # for attr in dir(dbResponse):
-        #     print(attr)
+        dbResponse = db.users.update_one(
+            {"_id": ObjectId(id)},
+            {"$set": {
+                "firstName":request.form["firstName"],
+                "lastName": request.form["lastName"]
+            }}
+        )
+        if dbResponse.modified_count == 1:
+            return Response(
+                response= json.dumps({"message":"user updated", "id":f"{id}"}),
+                status=200,
+                mimetype="application/json"
+            )
+
         return Response(
-            response= json.dumps(
-                {"message":"user created",
-                 "id":f"{dbResponse.inserted_id}"
-                 }),
-            status=200,
+            response= json.dumps({"message":"nothing to update"}),
+            status=404,
             mimetype="application/json"
         )
-    except Exception as ex:
-        print("################")
-        print(ex)
-        print("################")
 
-#############################################
+
+    except Exception as ex:
+        print(ex)
+        return Response(response= json.dumps({"message":"user not found"}), status=500, mimetype="application/json")
+
+# delete operation
+
+@app.route("/delete_users/<id>", methods=["DELETE"])
+def delete_users(id):
+    try:
+        dbResponse = db.users.delete_one({"_id": ObjectId(id)})
+        if dbResponse.deleted_count == 1:
+            return Response( response= json.dumps({"message":"user deleted", "id":f"{id}"}),status=200,mimetype="application/json")
+
+        return Response(response= json.dumps({"message":"user not found", "id":f"{id}"}),status=404,mimetype="application/json")
+
+    except Exception as ex:
+        return Response(response= json.dumps({"message":"sorry cannot delete the user"}), status=500, mimetype="application/json")
+
+
+###########################################
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
